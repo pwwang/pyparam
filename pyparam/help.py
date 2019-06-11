6922a6f6025047972c7f3e039883744aadec504b
pwwang <pwwang@pwwang.com>
2019-06-10 21:22:05 -0700

    Add docs

--- pyparam/help.py
+++ pyparam/help.py
@@ -25,23 +25,28 @@ class HelpItems(list):
 	An item of a help message without divisions.
 	For example, a line of description, usage or example.
 	"""
-	def __init__(self, *args, **kwargs):
+	def __init__(self, *args):
+		super(HelpItems, self).__init__()
 		for arg in args:
 			self.add(arg)
 
 	def add(self, item):
+		"""Add an item. Expected a list, if not will be .splitlines() for str.
+		"""
 		if not isinstance(item, list):
 			item = item.splitlines()
 		self.extend(item)
 		return self
 
 	def query(self, selector, regex = False):
+		"""Get the index of the matched item"""
 		for i, item in enumerate(self):
 			if _match(selector, item, regex):
 				return i
 		raise ValueError('No element found by selector: %r' % selector)
 
 	def after(self, selector, item, **kwargs):
+		"""Add an item after the item matched selector"""
 		index = self.query(selector, kwargs.pop('regex', False)) + 1
 		if not isinstance(item, list):
 			item = item.splitlines()
@@ -49,6 +54,7 @@ class HelpItems(list):
 		return self
 
 	def before(self, selector, item, **kwargs):
+		"""Add an item before the item matched selector"""
 		index = self.query(selector, kwargs.pop('regex', False))
 		if not isinstance(item, list):
 			item = item.splitlines()
@@ -56,14 +62,17 @@ class HelpItems(list):
 		return self
 
 	def replace(self, selector, content, **kwargs):
+		"""Replace the content of the item matching the selector"""
 		index = self.query(selector, kwargs.pop('regex', False))
 		self[index] = content
 		return self
 
 	def select(self, selector, **kwargs):
+		"""Select the content of the item matching the selector"""
 		return self[self.query(selector, kwargs.pop('regex', False))]
 
 	def delete(self, selector, **kwargs):
+		"""Delete the item matching the selector"""
 		del self[self.query(selector, kwargs.pop('regex', False))]
 		return self
 	remove = delete
@@ -86,6 +95,7 @@ class HelpOptions(HelpItems):
 		return '-' + name if len(name) <= 1 or name[1] == '.' else '--' + name
 
 	def addParam(self, param, aliases = None, ishelp = False):
+		"""Add a param"""
 		from . import OPT_POSITIONAL_NAME
 		aliases = aliases or []
 		aliases.append(param.name)
@@ -114,6 +124,7 @@ class HelpOptions(HelpItems):
 			paramtype, paramdesc))
 
 	def addCommand(self, params, aliases, ishelp = False):
+		"""Add a set of params"""
 		cmdtype = '[COMMAND]' if ishelp else ''
 		return self.add((
 			' | '.join(sorted(set(aliases), key = lambda alias: (len(alias), alias))),
@@ -121,7 +132,7 @@ class HelpOptions(HelpItems):
 			params['_desc'] if ishelp else params._desc
 		))
 
-	def add(self, item, aliases = None, ishelp = False):
+	def add(self, item, aliases = None, ishelp = False): # pylint: disable=arguments-differ
 		from . import Param, Params
 		if isinstance(item, Param):
 			self.addParam(item, aliases, ishelp)
@@ -142,6 +153,9 @@ class HelpOptions(HelpItems):
 
 	@property
 	def isMixed(self):
+		"""Tell whether options are mixed with short and long ones
+		check only if first option has a short name.
+		"""
 		firstopts = [item[0] for item in self
 			if item[0].startswith('-') and '|' not in item[0]]
 		if not firstopts or firstopts[0].startswith('--'):
@@ -171,12 +185,14 @@ class Helps(OrderedDict):
 	"""All sections of help"""
 
 	def query(self, selector, regex = False):
+		"""Get the  key matching the selector"""
 		for key in self:
 			if _match(selector, key, regex):
 				return key
 		raise ValueError('No section found by selector: %r' % selector)
 
 	def select(self, selector, regex = False):
+		"""Select the selection of the  key matching the selector"""
 		return self[self.query(selector, regex = regex)]
 
 	@staticmethod
@@ -191,24 +207,28 @@ class Helps(OrderedDict):
 			return HelpItems(*args, **kwargs)
 
 	def add(self, section, *args, **kwargs):
+		"""Add a selection"""
 		sectionobj = Helps._section(*args, **kwargs)
 		self[section] = sectionobj
 		return self
 
 	def before(self, selector, section, *args, **kwargs):
+		"""Add a section before one with titile matching selector"""
 		key = self.query(selector, kwargs.pop('regex', False))
 		sectionobj = Helps._section(*args, **kwargs)
 		self.insert_before(key, (section, sectionobj))
 		return self
 
 	def after(self, selector, section, *args, **kwargs):
+		"""Add a section after one with titile matching selector"""
 		key = self.query(selector, kwargs.pop('regex', False))
 		sectionobj = Helps._section(*args, **kwargs)
 		self.insert_after(key, (section, sectionobj))
 		return self
 
 	def delete(self, selector, regex = False):
-		key = self.query(selector, regex = False)
+		"""Delete the section titile matching selector"""
+		key = self.query(selector, regex = regex)
 		del self[key]
 		return self
 
@@ -216,7 +236,7 @@ class Helps(OrderedDict):
 
 	@property
 	def maxOptNameWidth(self, min_optdesc_leading = 5, max_opt_width = 36):
-
+		"""Calculate the width of option name and type"""
 		ret = 0
 		for item in self.values():
 			if not item or not isinstance(item, HelpOptions):
@@ -228,4 +248,3 @@ class Helps(OrderedDict):
 					if len(it[0] + it[1]) + min_optdesc_leading + 3 <= max_opt_width]
 			ret = ret if not itemlens else max(ret, max(itemlens))
 		return ret or max_opt_width
-
